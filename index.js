d435f9a33f61545079de4addd5488287aecbd9e9
Alex <shestmintsev@gmail.com>
2015-03-17 21:05:22 +0200

    first commit

--- index.js
+++ index.js
@@ -1,87 +1,26 @@
-angular.module('my.tabs', []);
-angular.module('my.tabs').directive('myTabs',function(){
+angular.module('myModal', []);
+
+angular.module('myModal').factory('myModalService',function($q,$rootScope, $constroller){
+
+    function open(paramsController,templateURL){
+
+        var $myScope = $rootScope.$new(true); // определение cвоего скоупа на основе $rootScope
+        $myScope.$result = $q.defer().promise;
+        var $myController = $controller(paramsController, {'$scope':$myScope}); // конроллер нужен  чтобы связать шаблон и скоуп
+        $templateRequest(templateUrl).then(function(element){
+
+        });
+
+        return $myScope.$result;
+    }
+
+
+
+
     return {
-        priority: 11,
-        restrict: 'A',
-        transclude: true,
-        controller: function ($scope) {
-            var tabsContainer = $scope.tabs = [];
+        open : open
+    }
 
-            this.addTab = function (tabInstance) {
-                tabsContainer.push(tabInstance);
-            };
-
-            this.removeTab = function (tab) {
-                var index = tabsContainer.indexOf(tab);
-                tabsContainer.splice(index, 1);
-            };
-
-            this.getTabs = function(){
-                return tabsContainer;
-            }
-        },
-        template: [
-            '<div>',
-            '<a style="margin-right: 20px;" href="#" ng-repeat="tab in tabs" ng-click="showTab(tab)">',
-            '{{tab.title}}',
-            '</a>',
-            '<div ng-transclude class="tabs-zone"></div>',
-            '</div>'
-        ].join(''),
-        scope: {},
-        link: function($scope, $element, $attr, tabsCtrl, transcludeFn){
-            var init = false;
-
-            $scope.$watch(
-                function () {
-                    return tabsCtrl.getTabs();
-                },
-                function (value) {
-                    if (value && init === false){
-                        init = true;
-                        $scope.showTab(value[0]);
-                    }
-                    $scope.tabs = value;
-                }
-            );
-
-            $scope.showTab = function (tab) {
-                $element.find('div').find('div').html(tab.transcludedContent.html());
-            };
-        }
-    };
 });
 
-angular.module('my.tabs').directive('myTab',function(){
-    return {
-        priority: 22,
-        restrict: 'A',
-        template: '',
-        require: '^^myTabs',
-        scope: {
-            myTab: "@"
-        },
-        transclude: true,
-        link: function($scope, element, attr, myTabsCtrl, $transclude) {
-                var id = Math.random();
-                var transcludedContent;
-
-                $transclude(function(clone, scope) {
-                    transcludedContent = clone;
-                });
-
-                myTabsCtrl.addTab({
-                    id: id,
-                    title: $scope.myTab,
-                    element: element,
-                    transcludedContent: transcludedContent
-                });
-
-                $scope.$on('$destroy', function() {
-                    tabsetCtrl.removeTab(scope);
-                });
-        }
-    };
-});
-
-
+angular.module('myModal').controller("MyCtrl",function ($scope) {});
